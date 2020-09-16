dca6ff917ff6bc00fa9a50ebee8d94d29eee91cb
Adwait <adwait.vyas@jupiter.money>
2020-09-16 22:59:04 +0530

    added functions

--- 07-functions.py
+++ 07-functions.py
@@ -2,11 +2,12 @@
 # You can pass data, known as parameters or arguments, into a function.
 # A function can return data as a result.
 # You define a function with def
-def my_function():
-    print("Hello from a function")
+def my_function(name):
+    print(f"Hello from {name}")
+    my_function(name)
 
 
 # This function will run only when invoked
 print('before invoking the function')
-my_function()
+my_function("adwait")
 print('after invoking the function')
---
+++ sum.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    print("Hi")
+    return a+b
+
+
+x = int(input("Enter a number:"))
+y = int(input("Enter another number:"))
+z = add(x, y)
+print(f"Sum is {z}")
